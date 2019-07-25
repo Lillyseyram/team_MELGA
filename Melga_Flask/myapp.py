20f8837dc018bc33a96839831222370db030328b
Lillyseyram <lillyseyram@gmail.com>
2019-07-25 21:48:51 +0100

    Final version

---
+++ Melga_Flask/myapp.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/contact')
+def contact():               
+    return render_template("contact.html")
+
+
+@app.route('/services')
+def services():
+    return render_template("services.html")
+
+@app.route('/portfolio')
+def portfolio():                                         
+    return render_template("portfolio.html")
+                                                     
+                                     
+@app.route('/blog')
+def blog():
+    return render_template("blog.html")
+
+
+                                                                        
+if __name__ == '__main__':
+    app.run(debug=True)
+
